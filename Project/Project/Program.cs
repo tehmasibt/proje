b21c6564fe9cb7e98319978f67fffb1e83fde406
thmasibtagiyev0@gmail.com <tahmasibmt@code.edu.az>
2024-02-13 16:47:45 +0400

    tagiyev

---
+++ Project/Project/Program.cs
@@ -0,0 +1,50 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.ComponentModel.Design;
+#region task-1
+//Console.WriteLine("Hello, World!");
+//int n = 8;
+//if (n % 2 == 0)
+//{
+//    Console.WriteLine("Ədəd cüdtür");
+//}
+//else
+//{
+//    Console.WriteLine("Ədəd təkdir" );
+//}
+#endregion
+
+#region task-2
+//int m= 21;
+//if (m%3==0&&m%7==0)
+//{
+//    Console.WriteLine("Bölünür");
+//}
+//  else
+//{
+//    Console.WriteLine("Bölünmür");
+//}
+#endregion
+
+#region task-3
+//int a = 16;
+//int b = 20;
+//if (a%2==0&&b%2==0)
+//{
+//    Console.WriteLine("a+b");
+//}
+//else
+//{
+//    Console.WriteLine("a-b");
+//}
+#endregion
+#region task-4
+//int t = 30;
+//if (t>15)
+//{
+//    Console.WriteLine("Hava istidir");
+//}
+//else
+//{
+//    Console.WriteLine("Hava soyuqdur");
+//}
+#endregion
